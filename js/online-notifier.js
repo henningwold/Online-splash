d7299c26095b18cb4be0b6a4a188548f8b0a1cf8
Tri Nguyen <mail@trimn.net>
2013-07-20 22:45:11 +0200

    Restyle the add notifier button.

--- js/online-notifier.js
+++ js/online-notifier.js
@@ -1,5 +1,5 @@
 
-var _addNotifier = ' + Add Notifier to Chrome';
+var _addNotifier = 'Add Notifier to Chrome';
 var _failedToAddNotifier = 'Adding Failed :(';
 var _addedNotifier = 'Added to Chrome <3';
 
@@ -24,9 +24,9 @@ $(function(){
 	var _isNotifierInstalled = chrome.app.isInstalled;
 
 	if (_isChrome && !_isNotifierInstalled) {
-		$('body').append('<button style="position:absolute;top:5pt;right:5pt;z-index:100;'+
+		$('body').append('<button style="position:absolute;top:0;right:0;z-index:100;'+
 			'text-align:center;font-size:13px;width:190px;'+
-			'font-weight:bold;height:28px;line-height:28px;margin:0;padding:0 6px;'+
+			'font-weight:bold;line-height:28px;margin:0;padding:0;font-size:12px;background:#222;color:#FFF;border:none;border-left:1px solid #444;border-bottom:1px solid #444;'+
 			'" id="install_notifier" onclick="_installNotifier();">'+
 			_addNotifier + '</button>');
 	}
