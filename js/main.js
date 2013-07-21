ce00f069a7ff619a78c37a3fd3de084fac1dd5bd
Tri Nguyen <mail@trimn.net>
2013-07-21 02:57:23 +0200

    Add day click functionality.

--- js/main.js
+++ js/main.js
@@ -1,60 +1,16 @@
-var lastClick;
-
-$(document).ready(function() {
-  var docLocHref    = document.location.href;
-  var pageRequest   = docLocHref.substring(docLocHref.indexOf('#') + 1, docLocHref.length);
+var lastClick
+, docLocHref    = document.location.href
+, pageRequest   = docLocHref.substring(docLocHref.indexOf('#') + 1, docLocHref.length)
+, open_window
+, change_page;
   
-  lastClick = $('#link_online');
 
-  switch (pageRequest) {
-    case 'join':                change_page('join');    break;
-    case 'mer':                 change_page('mer');     break;
-    case 'fadderukene':         change_page('fadderukene'); break;
-    case 'cal':                 change_page('cal');     break;
-    
-    default:
-      change_page('online');
-  }
 
-  $('nav a').click(function(event) {
-    switch (this.id) {
-      case 'link_online':       change_page('online');    break
-      case 'link_join':         change_page('join');    break;
-      case 'link_fadderukene':  change_page('fadderukene'); break;
-      case 'link_mer':          change_page('mer');     break;
-      case 'link_cal':          change_page('cal');     break;
-
-      default:
-        change_page('online');
-    }
-  });
-  var isIOS = navigator.userAgent.match(/(iPod|iPhone|iPad)/);
-  if (isIOS) {
-    $(document).on('touchstart', '.cal li', function() {
-      var isHidden = $('div', this).hasClass('hidden');
-      $('.calendar li div').addClass('hidden');
-      if (isHidden) {
-        $('div', this).removeClass('hidden');
-      }
-    });
-  }
-  else {
-    $(document).on('click', '.cal li', function() {
-      var isHidden = $('div', this).hasClass('hidden');
-      $('.calendar li div').addClass('hidden');
-      if (isHidden) {
-        $('div', this).removeClass('hidden');
-      }
-    });
-  }
-
-});
-
-function open_window(section_id) {
-  var guide_background    = document.createElement('div');
-  var guide               = document.createElement('div');
-  var guide_inner         = document.createElement('div');
-  var guide_close         = document.createElement('div');
+open_window = function (section_id) {
+  var guide_background    = document.createElement('div')
+  , guide                 = document.createElement('div')
+  , guide_inner           = document.createElement('div')
+  , guide_close           = document.createElement('div');
 
   guide_inner.innerHTML   = $(section_id).html();
   guide_close.innerHTML   = '<img src="graphics/close.png" alt="close">';
@@ -88,7 +44,7 @@ function open_window(section_id) {
   innerScroll = new iScroll(guide_inner);
 }
 
-var change_page = function(name) {
+change_page = function(name) {
   
   $('#content').fadeOut(180, function() {
     $('#content').html( $('#'+name).html()).fadeIn();
@@ -97,4 +53,60 @@ var change_page = function(name) {
   lastClick.removeClass('on');
   lastClick = $('#link_'+name);
   lastClick.addClass('on');
+}
+
+
+
+
+
+
+
+
+lastClick = $('#link_online');
+
+switch (pageRequest) {
+  case 'join':                change_page('join');    break;
+  case 'mer':                 change_page('mer');     break;
+  case 'fadderukene':         change_page('fadderukene'); break;
+  case 'cal':                 change_page('cal');     break;
+  default:
+    change_page('online');
+}
+
+$('nav a').click(function(event) {
+  switch (this.id) {
+    case 'link_online':       change_page('online');    break
+    case 'link_join':         change_page('join');    break;
+    case 'link_fadderukene':  change_page('fadderukene'); break;
+    case 'link_mer':          change_page('mer');     break;
+    case 'link_cal':          change_page('cal');     break;
+
+    default:
+      change_page('online');
+  }
+});
+
+$(document).on('click', '.cal-left ul li', function (e) {
+  $('#cal-content').html($('article', this).html());
+});
+
+
+var isIOS = navigator.userAgent.match(/(iPod|iPhone|iPad)/);
+if (isIOS) {
+  $(document).on('touchstart', '.cal li', function() {
+    var isHidden = $('div', this).hasClass('hidden');
+    $('.calendar li div').addClass('hidden');
+    if (isHidden) {
+      $('div', this).removeClass('hidden');
+    }
+  });
+}
+else {
+  $(document).on('click', '.cal li', function() {
+    var isHidden = $('div', this).hasClass('hidden');
+    $('.calendar li div').addClass('hidden');
+    if (isHidden) {
+      $('div', this).removeClass('hidden');
+    }
+  });
 }
